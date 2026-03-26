f45f8ae4e8250b7bd9beab829de7026437b8e201
Stephany Christine <118492298+phanys@users.noreply.github.com>
2026-03-26 20:03:54 -0300

    Update Carro.java

--- src/Carro.java
+++ src/Carro.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+public class Carro extends Veiculo {
+    private String janela;
 
-public class Carro {
+    public Carro(String marca, String modelo, String janela){
+        super(marca, modelo);
+        this.janela=janela;
+
+    }
+
+    public String getJanela(){
+        return janela;
+    }
 }
