eb806ef01b6869ae872855e6b42eb555e8ccb697
Stephany Christine <118492298+phanys@users.noreply.github.com>
2026-03-26 19:58:53 -0300

    Update Cachorro.java

--- src/Cachorro.java
+++ src/Cachorro.java
@@ -1,5 +1,13 @@
-public class Cachorro extends Animal{
-    public void fazerSom(){
-        System.out.println("AUAU");
+public class Carro extends Veiculo {
+    private String janela;
+
+    public Carro(String marca, String modelo, String janela){
+        super(marca, modelo);
+        this.janela=janela;
+
+    }
+
+    public String getJanela(){
+        return janela;
     }
 }
