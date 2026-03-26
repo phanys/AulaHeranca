dce82c91a951ac429865c13f75d982400d6dc093
Stephany Christine <118492298+phanys@users.noreply.github.com>
2026-03-26 20:01:30 -0300

    Update Veiculo.java

--- src/Veiculo.java
+++ src/Veiculo.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class Veiculo {
+    private String marca;
+    private String modelo;
+
+    public Veiculo(String marca, String modelo){
+        this.marca=marca;
+        this.modelo=modelo;
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public String getModelo(){
+        return modelo;
+    }
+
+    public void exibirInformacoes(){
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+    }
 }
